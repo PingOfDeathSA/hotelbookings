24f9b166e34c991961689ee1e2296a4a1a4e76eb
PingOfDeathSA <ronaldnt8@gmail.com>
2023-03-13 09:14:25 +0200

    switched to hotel

--- app.js
+++ app.js
@@ -1,443 +1,187 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Publisheble_key = "pk_test_51MefisHx2PKqOmHl3exdk4uRzsea8wDjVR4qzUdwyLj6kwtzoTIAbijAc34tAAJlUPk9SViZqor5GgmojgwVOF1b00irkXoVM0"
+const Secret_key = "sk_test_51MefisHx2PKqOmHlE7Xw1vOBCro6vE1UjAjLGxVHcUqUEHrVScAB9lMIARyY3WGHIKwW3BoY6xfuRbGufxeAilUM00xEHCy8J6"
 // const date = require(__dirname + "/date.js");
 mongoose.set('strictQuery', true);
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
-mongoose.connect('mongodb+srv://PingOfDeathSA:Ronald438@cluster0.kqlfkdc.mongodb.net/SchoolDB');
-var StudentNumber = Math.random();
-StudentNumber = Math.floor(StudentNumber *900)+100;
-var currentSN = "2023"+StudentNumber
-// console.log(currentSN);
 
 
-const Grade_PropertiesSchema = mongoose.Schema({
-  TeacherName: {
-    type: String,
-    required: [true, "check Teacher title Enter Title"],
-  },
-  Teacher_Profile_Picture: {
-    type: String,
-    required: [true, "Please provide a valid link for the teacher's profile picture"]
-  },
-    address: {
-      type: String,
-      required: [true, "Check address missing"]
-    },
-    email_adsress:  {
-      type: String,
-      required: [true, "Check Email missing"]
-    },
-    teacher_number: {
-      type: String,
-      required: [true, "Check Teacher Number missing missing"]
-    },
-    Title:{
-      type:String,
-      minlength: 2,
-      maxlength: 2,
-      required: [true, "check Teacher title Enter Title"],
-    },
-      grade_number: 
-      {
-        type: String || Number,
-        min:2,
-        max: 2,
-        required: [true, "check Grade number"],
-        
+// conecting with cloud database
+mongoose.connect('mongodb+srv://PingOfDeathSA:Ronald438@cluster0.kqlfkdc.mongodb.net/HotelDB');
+// Hotel number generator
+var HotelNumber = Math.random();
+HotelNumber = Math.floor(HotelNumber *6)+1;
+var current_Key = "2023"+HotelNumber
+// data structure requirements
+const hotelSchema = new mongoose.Schema({
+    Hotel_key: {type:Number,requred:true},
+  name: { type: String, required: true },
+  picture1: { type:String, required: true},
+  picture2: { type:String, required: true},
+  picture3: { type:String, required: true},
+  rating: { type: Number, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  available_dates: { type: [Date], required: true},
+});
+// linkinng  the data with the structure schema
+const HotelModel = mongoose.model('Hotel', hotelSchema);
+// adding informaitin inside the model
+const HotelSave = new HotelModel({
+    Hotel_key: current_Key,
+   name: "Hotel D",
+   picture1: "https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  picture2:  "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  picture3:  "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  rating: { type: Number, required: true },
+    rating: 4.3,
+    location: "San Francisco",
+    price: 250,
+    available_dates: [
+      "2023-03-15",
+      "2023-03-16",
+      "2023-03-17"
+    ]
+  });
+// Building APIs
+// // app.get() his is an HTTP method used to retrieve data from a server or database. In the context of an API
+
+app.get("/creat-pay")
+
+
+
+
+app.get("/",  (req, res) => {
+  HotelModel.find(
+    {  }, //filtter
     
-      },
-      Gender: {
-        type: String,
-        minlength: 1,
-        maxlength: 1,
-        required: [true, " Check Gender charector"]
-    },
-    Subject: {
-      type: String,
-      
-      required: [true, " Check Subject missing"]
-  },
-    
-  
-});
-const Grade_Propertiesmodel = mongoose.model("Grade_ProCollection", Grade_PropertiesSchema);
-
-const LearnersSchema = mongoose.Schema({ 
-  GRADEProps: {
-    type: mongoose.Schema.Types.ObjectId, // Reference field
-    ref: 'Grade_ProCollection' // Name of the referenced schema collection
-  },
-Student_Number: {
-  type: Number,
-  required: [true, "Check Student Name missing"]
-},
-  Fist_Name: {
-    type: String,
-    required: [true, "Check First Name missing"]
-  },
-  Last_Name: {
-    type: String,
-    required: [true, "Check Last Name missing"]
-  },
-  grade_number: 
-  {
-    type: String || Number,
-    min: 2,
-    max: 2,
-    required: [true, "check Grade number"],
-    
-
-  },
-  Gender: {
-      type: String,
-      minlength: 1,
-      maxlength: 1,
-      required: [true, " Check Gender charector"]
-  },
-  
-  Leaner_Profile_Picture: {
-    type: String,
-    required: [true, "Check profile link missing"]
-  },
-  address: {
-    type: String,
-    required: [true, "Check address missing"]
-  },
-  email_adsress:  {
-    type: String,
-    required: [true, "Check Email missing"]
-  },
-
-  // LearnerT: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher'}
-});
-
-const Learnermodel = mongoose.model("LearnerCollection", LearnersSchema);
-
-const  Grade_PropertiesSave = new Grade_Propertiesmodel({
-  TeacherName: "Chad Hitchcock",
-  Title:"Mr",
-  address: "Diphagane",
-  teacher_number: "t5",
-  Teacher_Profile_Picture: "https://images.pexels.com/photos/9185400/pexels-photo-9185400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  Subject:"English",
-  Gender:"M",
-  grade_number:"9C",
-  email_adsress:"ronald@gmail.com",
-
-
-});
-
-const learnerSave = new Learnermodel(
-{
-  // LearnerT: learnerTeacher,
-  Gender:"F",
-
-  Leaner_Profile_Picture:"https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  address:"Jane Furse",
-  
-  email_adsress:
-  "Goad-C@school.co.za",
-  
-  grade_number:
-  "9F",
-  Fist_Name: "Sammy",
-  Last_Name: "Phahlamohlaka",
-  Student_Number:currentSN
-
-
-});
-
-
-// Model query for teacher prolile
-
-
-
-let Teacher_subjects;
-
-Grade_Propertiesmodel.find(function (err, Teachers) {
-  if (err) {
-    console.log(err);
-  } else {
-    Teachers.forEach(function (Teacher) {
-      Teacher_subjects = Teacher.Subject;
-       console.log(Teacher_subjects);
-    });
-    // Here you can use the current_learner variable
-    // console.log("Current learner is: " + current_learner_Student);
-  }
-});
-
-
-let Teacher_Grade;
-Grade_Propertiesmodel.find(function (err, Teachers) {
-  if (err) {
-    console.log(err);
-  } else {
-    Teachers.forEach(function (Teacher) {
-      Teacher_Grade = Teacher.grade_number;
-       console.log(Teacher_Grade);
-    });
-    // Here you can use the current_learner variable
-    // console.log("Current learner is: " + current_learner_Student);
-  }
-});
-
-app.get('/', (req, res) => {
-  // res.sendfile('Mylearners.html'); 
-
-  Grade_Propertiesmodel.find(
-    
-    { grade_number: ["9C","9F"] },
-    
-    function (err, leanerDetails) {
+    function (err, Details_of_Hotel) {
 
     if (err) {
       console.log(err)
       
-    } else { console.log(leanerDetails)}
+    } else { console.log(Details_of_Hotel)}
 
-    res.render("list", {listTitle: "Today", Learn: leanerDetails});
+    res.render("MyHotels", {listTitle: "Today", Hotel: Details_of_Hotel,
+
+ 
+  
+  });
+  });
 
 
   });
+  app.get('/Bookings.html', (req, res) => {
+    const checkedid = req.body.selectedLocation;
+    console.log("Selected Location: ", checkedid);
   
-})
-
-// leaners stats model count
-app.get('/', (req, res) => {
-  // res.sendfile('Mylearners.html'); 
-
-  Learnermodel.find(
-    
-    { grade_number: ["9C","9F"] },
-    
-    function (err, leanerDetails) {
-
-    if (err) {
-      console.log(err)
+    HotelModel.find(
       
-    } else { console.log(leanerDetails)}
-
-    res.render("list", {listTitle: "Today", Learn: leanerDetails});
-
-
-  });
-  
-})
-
-
-
-// Model query for Leaners all
-// querying leaner by grade
-app.get('/Mylearners.html', (req, res) => {
-  // res.sendfile('Mylearners.html'); 
-
-  Learnermodel.find(
-    
-    { grade_number: ["9C","9F"] },
-    
-    function (err, leanerDetails) {
-
-    if (err) {
-      console.log(err)
+      { location: checkedid },
       
-    } else { console.log(leanerDetails)}
-
-    res.render("learners", {listTitle: "Today", Learn: leanerDetails});
-   
-
-  });
-
-  
-});
-
-// counting the numberr of learners
-
-app.get('/students', function(req, res) {
-  Learnermodel.find({}, function(err, students) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('students', { students: students });
-    }
-  });
-});
-
-
-
-// querying leaner by grade
-
-
-
-// querying Garde Proparties by informtion
-
-app.get("/", function(req, res) {
-  
-  
-    Learnermodel.find(
-      
-      { grade_number: ["9C","9F"] },
-      
-      function (err, leanerDetails) {
+      function (err, Details_of_Hotel) {
   
       if (err) {
         console.log(err)
         
-      } else { console.log(leanerDetails)}
+      } else { console.log(Details_of_Hotel)}
   
-      res.render("list", {listTitle: "Today", Learn: leanerDetails});
-  
-  
-    })
-  // const day = date.getDate();
-  
-  
-  });
-  
-
-
-
-// adding new leaner
-app.post("/", function(req, res){
-
-  const   Fist_Name1 = req.body.newItem;
-  const learnerSave = new Learnermodel(
-    {
-      
-      // LearnerT: learnerTeacher,
-      Gender:"F",
-    
-      Leaner_Profile_Picture:"https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      address:"Jane Furse",
-      
-      email_adsress:
-      "Goad-C@school.co.za",
-      
-      grade_number:
-      "9F",
-      Fist_Name: Fist_Name1,
-      Last_Name: "Phahlamohlaka",
-      Student_Number:currentSN
-    
-    
+      res.render("Bookings", {listTitle: "Today", Hotel: Details_of_Hotel});
     });
-
-//  learnerSave.save().then(() => res.redirect("/Mylearners.html"));
-
-});
-// deleting leaner
-app.post("/delete", function (req, res) {
-  const checkedid = req.body.checkbox;
-  Learnermodel.findByIdAndRemove(checkedid, function (err, docs) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Removed User : ", docs);
-      res.redirect("/Mylearners.html");
-    }
   });
-});
- const main = "Ronald438";
- const main2 = "PingOfDeathSA";
 
-// app.get("/work", function(req,res){
-//   res.render("list", {listTitle: "Work List", newListItems: workItems});
-// });
+  // Getting user input API
+  app.post("/", function(req, res){
 
-// database validation
-app.listen(5000, function() {
-
+    const   Fist_Name1 = req.body.newItem;
+    const learnerSave = new Learnermodel(
+      {
+        
+        // LearnerT: learnerTeacher,
+        Gender:"F",
+      
+        Leaner_Profile_Picture:"https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        address:"Jane Furse",
+        
+        email_adsress:
+        "Goad-C@school.co.za",
+        
+        grade_number:
+        "9F",
+        Fist_Name: Fist_Name1,
+        Last_Name: "Phahlamohlaka",
+        Student_Number:currentSN
+      });
+  //  learnerSave.save().then(() => res.redirect("/Mylearners.html"));
+  });
+  app.post("/search", function (req, res) {
+    const checkedid = req.body.selectedLocation;
+    console.log("Selected Location: ", checkedid);
+    HotelModel.find(
     
-  
-  console.log("Server started on port 5000");
-  
-  
-});
-// Learnermodel.find(
-  
-//   function (err, learners) {
+      { location: checkedid },
+      function (err, Details_of_Hotel) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Details of Hotel: ", Details_of_Hotel);
+          res.render("MyHotels", { listTitle: "Today", Hotel: Details_of_Hotel });
+        }
+      }
+    );
+  });
+
+// validator
+// let current_learner_Student;
+
+// Learnermodel.find(function (err, learners) {
 //   if (err) {
-//       console.log(err);
+//     console.log(err);
 //   } else {
 //     learners.forEach(function (learner) {
-//         var Curren_leaner = learner.Student_Number 
-//       console.log(
-//           learner.Student_Number 
-//             );
-           
-  
+//       current_learner_Student = learner.Student_Number;
+//       // console.log(current_learner_Student);
+//     });
+//     // Here you can use the current_learner variable
+//     // console.log("Current learner is: " + current_learner_Student);
+//   }
 // });
+// const fist_Name_VALIDATOR = "Sammy";
+// const last_Name_VALIDATOR = "Phahlamohlaka";
+// const STN_validator = current_learner_Student
+
+
+
+
+
+
+
+
+// Learnermodel.find({ Last_Name: last_Name_VALIDATOR },{ Fist_Name: fist_Name_VALIDATOR },{ Student_Number: STN_validator }, function (err, learners) {
+//   if (learners.some((learner) => learner.Last_Name === last_Name_VALIDATOR) || learners.some((learner) => learner.Fist_Name === fist_Name_VALIDATOR) && learners.some((learner) => learner.Student_Number === STN_validator)) {
+//     console.log("Learner already exists");
+//   } else {
+//     //  console.log("Learner already add");
+//       learnerSave.save().then(() => console.log('Learner added'));
 //   }
 // });
 
 
 
 
-// validator
-let current_learner_Student;
-
-Learnermodel.find(function (err, learners) {
-  if (err) {
-    console.log(err);
-  } else {
-    learners.forEach(function (learner) {
-      current_learner_Student = learner.Student_Number;
-      // console.log(current_learner_Student);
-    });
-    // Here you can use the current_learner variable
-    // console.log("Current learner is: " + current_learner_Student);
-  }
-});
-const fist_Name_VALIDATOR = "Sammy";
-const last_Name_VALIDATOR = "Phahlamohlaka";
-const STN_validator = current_learner_Student
+  
 
 
 
 
-
-
-
-
-Learnermodel.find({ Last_Name: last_Name_VALIDATOR },{ Fist_Name: fist_Name_VALIDATOR },{ Student_Number: STN_validator }, function (err, learners) {
-  if (learners.some((learner) => learner.Last_Name === last_Name_VALIDATOR) || learners.some((learner) => learner.Fist_Name === fist_Name_VALIDATOR) && learners.some((learner) => learner.Student_Number === STN_validator)) {
-    console.log("Learner already exists");
-  } else {
-    //  console.log("Learner already add");
-      learnerSave.save().then(() => console.log('Learner added'));
-  }
-});
-
-
-
-
-
-
-// Deleting 
-// Learnermodel.deleteOne(
-//     { _id: "63eca4d68cb526dee3dle"}, {Gender: "M"},   
-//      function (err) {
-//              if (err) {console.log(err) 
-//             } else {
-//                 console.log("Delete is Succesful")
-        
-//              }
-            
-//             }
-//    )
-//  Grade_PropertiesSave.save().then(() => console.log('Grade_Properties added'));
-
-  //  learnerSave.save().then(() => console.log('Learner added'));
-
-
-
+//   geting the local server up and runing
+  app.listen(2000, function() {
+    console.log("Server started on port 2000");     
+  });
 
